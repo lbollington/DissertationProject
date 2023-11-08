f790d4bae3fe23dc9336e8bc0541e5a13adeaf50
lbollington <lukebollington10@gmail.com>
2023-11-08 15:55:33 +0000

    Fixed deprecation in AdminActivity

--- app/src/main/java/com/example/test/AdminActivity.java
+++ app/src/main/java/com/example/test/AdminActivity.java
@@ -2,107 +2,66 @@ package com.example.test;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
 import android.widget.TextView;
 
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import com.google.firebase.auth.FirebaseAuth;
-
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Objects;
 
 
 public class AdminActivity extends AppCompatActivity {
     TextView notificationAppNum, notificationAccNum;
     FirebaseAuth firebaseAuth;
-    FirebaseFirestore firestore;            //variables
+    FirebaseFirestore firestore;
     String userId;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);        //onCreate method to set content view
-        //FullName = findViewById(R.id.nameDisplay);
-        //Email = findViewById(R.id.emailDisplay);
+        setContentView(R.layout.activity_admin);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        firestore = FirebaseFirestore.getInstance();            //initialise variables
-        userId = firebaseAuth.getCurrentUser().getUid();
+        firestore = FirebaseFirestore.getInstance();
+        userId = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
         notificationAppNum = findViewById(R.id.notificationAppNumber);
         notificationAccNum = findViewById(R.id.notificationAccNumber);
-        //notificationPassNum = findViewById(R.id.notificationPassNumber);
 
         DocumentReference accRequests = firestore.collection("accountRequestsNumber").document("Lo8vvjq2m97ySrs2L1HA");
-        accRequests.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override                                                                                               //reference to counter
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                if(documentSnapshot != null){
-                    notificationAccNum.setText(documentSnapshot.getLong("NumOfAccRequests").toString());
-                }
+        accRequests.addSnapshotListener(this, (documentSnapshot, e) -> {
+            if(documentSnapshot != null){
+                notificationAccNum.setText(String.format(String.valueOf(documentSnapshot.getLong("NumOfAccRequests"))));
             }
         });
 
         DocumentReference appRequests = firestore.collection("appointmentRequestsNumber").document("WZMyCwUdyT6TXLvcU28Q");
-        appRequests.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                if(documentSnapshot != null){                                                                       //reference to counter
-                    notificationAppNum.setText(documentSnapshot.getLong("NumOfAppRequests").toString());
-                }
+        appRequests.addSnapshotListener(this, (documentSnapshot, e) -> {
+            if(documentSnapshot != null){
+                notificationAppNum.setText(String.format(String.valueOf(documentSnapshot.getLong("NumOfAppRequests"))));
             }
         });
 
-
-        //name + email
-        /*DocumentReference documentReference= firestore.collection("approvedUsers").document(userId);
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                if(documentSnapshot != null){
-                    FullName.setText(documentSnapshot.getString("FullName"));
-                    Email.setText(documentSnapshot.getString("UserEmail"));
-                }
-            }
-        });*/
-
         Button addPatient = findViewById(R.id.btnAddPatient);
-        addPatient.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {                    //on-click listener takes the admin to account notifications
-                FirebaseAuth.getInstance();
-                startActivity(new Intent(AdminActivity.this, AccountNotifications.class));
-            }
+        addPatient.setOnClickListener(view -> {
+            FirebaseAuth.getInstance();
+            startActivity(new Intent(AdminActivity.this, AccountNotifications.class));
         });
 
         Button schedule = findViewById(R.id.btnSchedule);
-        schedule.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {                //on-click listener takes the admin to appointment notifications
-                FirebaseAuth.getInstance();
-                startActivity(new Intent(AdminActivity.this, AppointmentNotifications.class));
-            }
+        schedule.setOnClickListener(view -> {
+            FirebaseAuth.getInstance();
+            startActivity(new Intent(AdminActivity.this, AppointmentNotifications.class));
         });
 
-
         Button logout = findViewById(R.id.logoutAdminBtn);
-        logout.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {                    //on-click listener logs out the admin
+        logout.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(AdminActivity.this, Login.class));
             finish();
-        }
-    });
+        });
 }
 }
